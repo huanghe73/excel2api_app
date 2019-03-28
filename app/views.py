694584abf7e35a3b7c04082531eafff7cd34149f
Austin Huang <huanghe73@gmail.com>
2019-03-28 17:31:54 -0500

    example from fab works for file upload

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from flask import render_template
 from flask_appbuilder.models.sqla.interface import SQLAInterface
 from flask_appbuilder import ModelView
 from app import appbuilder, db
+from app.models import Project, ProjectFiles
 
 """
     Create your Views::
@@ -17,6 +18,31 @@ from app import appbuilder, db
     appbuilder.add_view(MyModelView, "My View", icon="fa-folder-open-o", category="My Category", category_icon='fa-envelope')
 """
 
+class ProjectFilesModelView(ModelView):
+    datamodel = SQLAInterface(ProjectFiles)
+
+    label_columns = {'file_name': 'File Name', 'download': 'Download'}
+    add_columns = ['file', 'description','project']
+    edit_columns = ['file', 'description','project']
+    list_columns = ['file_name', 'download']
+    show_columns = ['file_name', 'download']
+
+
+class ProjectModelView(ModelView):
+    datamodel = SQLAInterface(Project)
+    related_views = [ProjectFilesModelView]
+
+    show_template = 'appbuilder/general/model/show_cascade.html'
+    edit_template = 'appbuilder/general/model/edit_cascade.html'
+
+    add_columns = ['name']
+    edit_columns = ['name']
+    list_columns = ['name', 'created_by', 'created_on', 'changed_by', 'changed_on']
+    show_fieldsets = [
+        ('Info', {'fields': ['name']}),
+        ('Audit', {'fields': ['created_by', 'created_on', 'changed_by', 'changed_on'], 'expanded': False})
+    ]
+
 """
     Application wide 404 error handler
 """
@@ -26,4 +52,5 @@ def page_not_found(e):
 
 db.create_all()
 
-
+appbuilder.add_view(ProjectModelView, "List Projects", icon="fa-table", category="Projects")
+appbuilder.add_view_no_menu(ProjectFilesModelView)
